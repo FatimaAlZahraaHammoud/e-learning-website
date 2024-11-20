cc40dad6d3f6e9f859925df0aed0fe646d63cf28
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-11-20 22:48:43 +0200

    Fix getCoursesForInstructor API

--- Server/getCoursesForInstructor.php
+++ Server/getCoursesForInstructor.php
@@ -1,23 +1,45 @@
 <?php
 
-    include "connection.php";
-
+    include "connection.php"; 
+    require "vendor/autoload.php";
+    
     header('Content-Type: application/json');
 
     header("Access-Control-Allow-Origin: http://localhost:3000");
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS, REQUEST");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-    $instructor_id = $_POST["instructor_id"] ?? null;
+    use Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
 
-    if (!$instructor_id) {
+    $secretKey = "FatimaSecretKeyy";
+    $headers = getallheaders();
+    $jwt = $headers["Authorization"];
+
+    $key = new Key($secretKey, "HS256");
+    $payload = JWT::decode($jwt, $key);
+
+    if($payload->role != "instructor") {
+        http_response_code(401);
+
+        echo json_encode([
+        "message"=> "Unauthorized"
+        ]);
+
+        return;
+    }
+
+
+    $id = $payload->userId;
+
+    if (!$id) {
         http_response_code(400);
         echo json_encode(['error' => 'Instructor ID is required']);
         exit;
     }
 
     $coursesQuery = $connection->prepare("select c.*, u.name as instructor_name from courses c INNER JOIN users u on c.instructor_id = u.user_id where c.instructor_id = ?");
-    $coursesQuery->bind_param("i", $instructor_id);
+    $coursesQuery->bind_param("i", $id);
     $coursesQuery->execute();
     $result = $coursesQuery->get_result();
 
